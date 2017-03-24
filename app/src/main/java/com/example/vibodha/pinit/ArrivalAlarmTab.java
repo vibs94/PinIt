3a5853a091e3fed3b6243acd970449e59a03dfbe
vibs94 <vibodha.14@cse.mrt.ac.lk>
2017-03-24 19:08:20 +0530

    init push

---
+++ app/src/main/java/com/example/vibodha/pinit/ArrivalAlarmTab.java
@@ -0,0 +1,23 @@
+package com.example.vibodha.pinit;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by vibodha on 3/23/17.
+ */
+
+public class ArrivalAlarmTab extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.arrival_alarm_tab, container, false);
+
+    }
+
+}
